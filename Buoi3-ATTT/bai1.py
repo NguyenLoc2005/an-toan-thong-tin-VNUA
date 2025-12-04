6f379a4e58624a11232c081bed3169e40dd61f42
Humble Kind <nxl522005@gmail.com>
2025-12-03 23:50:43 -0800

    Add files via upload
    Buoi 3

---
+++ Buoi3-ATTT/bai1.py
@@ -0,0 +1,12 @@
+#Bai 1
+x = int(input("Nhập vào x: "))
+la_so_nguyen_to = False
+for i in range(2,x):
+  if x%i==0:
+    la_so_nguyen_to = False
+    print(x, "không là số nguyên tố")
+    break
+  else:
+    la_so_nguyen_to = True
+if la_so_nguyen_to==True:
+  print(x, "là số nguyên tố")
